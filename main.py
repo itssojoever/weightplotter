383e97ad40fce10737fbe41776e02feff174b9e7
JoeEnterAUsername <144183809+JoeEnterAUsername@users.noreply.github.com>
2024-03-27 23:07:51 +0000

    Functional save of some data to CSV

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import csv
+import os
 from datetime import datetime
 from tkinter import ttk
 from tkcalendar import Calendar
@@ -9,14 +11,33 @@ root.geometry("400x400")
 root.title("Weightplotter")
 root.iconname(None)
 
+def loadData():
+    if os.path.isfile("config.ini"):
+        pass
+    else:
+        with open("config.ini", "w") as f:
+            pass
+
 def saveData():
-    messagebox.showinfo("Saved", message="Date saved")
-    pass
+    inputtedDate = cal.get_date()
+    inputtedCurrentWeight = weightCurrentInput.get()
+    
+    with open("data.csv", "a", newline="") as csvfile:
+        writer = csv.writer(csvfile)
+        row = (inputtedDate, inputtedCurrentWeight)
+        writer.writerow(row)
+        csvfile.close()
+        
+
+
 
 
 #frames
-Frame1 = tk.LabelFrame(root)
-Frame2 = tk.LabelFrame(root)
+Frame1 = ttk.LabelFrame(root)
+Frame2 = ttk.LabelFrame(root)
+Frame3 = ttk.LabelFrame(root)
+Frame4 = ttk.LabelFrame(root)
+Frame5 = ttk.LabelFrame(root)
 
 #Calendar function
 currentDate = datetime.now()
@@ -29,22 +50,54 @@ cal = Calendar(Frame1, selectmode = "day",
                month = currentMonth, 
                day = currentDay)
 
-inputtedDate = cal.get_date()
-
-print()
-
-
 #Frame1
 l1 = ttk.Label(Frame1, font="helvetica, 12", text="Select the date: ")
-CalendarSave = ttk.Button(Frame1, text="Save date", command=lambda: saveData())
 
+#Frame2
+l2 = ttk.Label(Frame2, font="helvetica, 12", text="Select weight measurement: ")
+weightoption_list = ["kg", "lbs"]
+weightMeasurementInputted = tk.StringVar(root)
+weightMeasurementInputted.set("Select a measurement")
+weightMeasurementOption = tk.OptionMenu(Frame2, weightMeasurementInputted, *weightoption_list)
 
-#gridding/packing
-cal.grid(row=0, column=1)
+#Frame3
+l3 = ttk.Label(Frame3, font="helvetica, 12", text="Input desired weight: ")
+weightDesiredInput = ttk.Entry(Frame3, font="helvetica, 12")
+
+#Frame4
+l4 = ttk.Label(Frame4, font="Helvetica, 12", text="Input weight")
+weightCurrentInput = ttk.Entry(Frame4, font="helvetica, 12")
+
+#Frame5
+saveButton = ttk.Button(Frame5, text="Save data", command=lambda: saveData())
+
+#gridding/packing ---------
+
 #Frame1
 Frame1.grid(row=0, column=0)
 l1.grid(row=0, column=0)
-CalendarSave.grid(row=1, column=1)
+cal.grid(row=0, column=1)
+
+#Frame2
+Frame2.grid(row=1, column=0)
+l2.grid(row=0, column=0)
+weightMeasurementOption.grid(row=0, column=1)
+
+#Frame3
+Frame3.grid(row=2, column=0)
+l3.grid(row=0, column=0)
+weightDesiredInput.grid(row=0, column=1)
+
+#Frame4
+Frame4.grid(row=3, column=0)
+l4.grid(row=0, column=0)
+weightCurrentInput.grid(row=0, column=1)
+
+#Frame5
+Frame5.grid(row=4, column=0)
+saveButton.grid(row=1, column=1)
+
+
 
 
 
